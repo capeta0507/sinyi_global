3f8d267db7fe2522249d83da6f9e040666d7e860
詹政勳 <nintendof1@gmail.com>
2020-02-28 11:39:42 +0800

    query

--- pages/sellcallquery.js
+++ pages/sellcallquery.js
@@ -9,13 +9,8 @@ const Index = () => (
     <Link href={`/sellquery?rent=7500&agent=johnny`}>
       <a target='_top'>A方案租金</a>
     </Link>
-<<<<<<< HEAD
-		<Link href={`/sellquery?rent=8000&agent=david`}>
-      <a>B方案租金</a>
-=======
     <Link href={`/sellquery?rent=8000&agent=david`}>
       <a target='_top'>B方案租金</a>
->>>>>>> 34cc5a6b19315e1d5889199877eb83d3d2e72111
     </Link>
     <p>建議採用 hook useRouter 方法</p>
   </div>
